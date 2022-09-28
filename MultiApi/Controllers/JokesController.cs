ac514257064d4112cdf79f23d117a821abf2f4b6
Kristijan Prematarov <Kristijan.Prematarov@opengi.mk>
2022-09-28 22:26:55 +0200

    Prepare API

---
+++ MultiApi/Controllers/JokesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MultiApi.Models;
+
+namespace MultiApi.Controllers
+{
+    [ApiController]
+    [Route("api/jokes")]
+    public class JokesController
+    {
+        [HttpGet]
+        public Joke Get()
+        {
+            Random rand = new Random();
+            int toSkip = rand.Next(0, 10);
+            Joke joke = Repository()
+                .Skip(toSkip)
+                .Take(1)
+                .FirstOrDefault();
+            return joke;
+        }
+
+        public List<Joke> Repository()
+        {
+            List<Joke> jokeList = new List<Joke>
+            {
+                new (){Name = "RATTLE SNAKE", Text = "Two men are hiking through the woods when one of them cries out, “Snake! Run!” His companion laughs at him. “Oh, relax. It’s only a baby,” he says. “Don’t you!", Category="Animal" },
+                new (){Name = "HORSE RIDER", Text = "To be or not to be a horse rider, that is equestrian. —Mark Simmons, comedian", Category="Animal" },
+                new (){Name = "POSTURE CAT", Text = "What did the grandma cat say to her grandson when she saw him slouching? A: You need to pay more attention to my pawsture.", Category="Animal" },
+                new (){Name = "HE CAN DO IT HIMSELF", Text = "It was my first night caring for an elderly patient. When he grew sleepy, I wheeled his chair as close to the bed as possible and, using the techniques I’d...", Category="Dockor" },
+                new (){Name = "ON THE BADGE", Text = "My 85-year-old grandfather was rushed to the hospital with a possible concussion. The doctor asked him a series of questions: “Do you know where you are?” “I’m at Rex Hospital.”...", Category="Dockor" },
+                new (){Name = "THE NURSE HAS MY TEETH", Text = "As a brain wave technologist, I often ask postoperative patients to smile to make sure their facial nerves are intact. It always struck me as odd to be asking this...", Category="Dockor" },
+                new (){Name = "GLUTEN ATTACK", Text = "Guy staring at an ambulance in front of Whole Foods: “Somebody must have accidentally eaten gluten.”", Category="Food" },
+                new (){Name = "MORNING TEA", Text = "What has T in the beginning, T in the middle, and T at the end? A: A teapot.", Category="Food" },
+                new (){Name = "MAKE ME A SANDWICH", Text = "My husband and I were daydreaming about what we would do if we won the lottery. I started: “I’d hire a cook so that I could just say, ‘Hey, make...", Category="Marriage" },
+                new (){Name = "SELL IT", Text = "As my wife and I prepared for our garage sale, I came across a painting. Looking at the back, I discovered that I had written “To my beautiful wife on...", Category="Marriage" }
+            };
+
+            return jokeList;
+        }
+    }
+}
