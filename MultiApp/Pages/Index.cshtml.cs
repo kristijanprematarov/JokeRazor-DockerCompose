b7af60e58a9bdc5fa66677a6aaa9a7efc3b77337
Kristijan Prematarov <Kristijan.Prematarov@opengi.mk>
2022-09-28 22:46:12 +0200

    Prepare the web app

--- MultiApp/Pages/Index.cshtml.cs
+++ MultiApp/Pages/Index.cshtml.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using MultiApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace MultiApp.Pages
@@ -17,9 +20,18 @@ namespace MultiApp.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
+            using var httpClient = new HttpClient();
+            var request = new HttpRequestMessage();
+            request.RequestUri = new Uri("http://localhost:63385/api/jokes");
+            request.Method = HttpMethod.Get;
 
+            var response = await httpClient.SendAsync(request);
+
+            var stream = await response.Content.ReadAsStreamAsync();
+
+            ViewData["Joke"] = await JsonSerializer.DeserializeAsync<Joke>(stream, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
         }
     }
 }
